8c70c328fbe5a563e6ca4baed73fc5d6ed7482b9
SuminJN <wjstnals1211@gmail.com>
2023-06-15 13:49:26 +0900

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by 전수민 on 2023/06/15.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
